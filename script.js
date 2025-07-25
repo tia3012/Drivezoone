b5cfa2dbcc6bedaaaba45da6b7614c18333b1650
tia3012 <tia301295@gmail.com>
2025-07-25 18:08:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
   if (btn) {
     btn.addEventListener("click", function (event) {
-      event.preventDefault(); // يمنع التنقل المؤقت
-
-      // ✅ تغيير نص الزر
+      event.preventDefault(); 
       btn.innerText = "Redirecting...";
-
-      // ✅ تغيير لونه كمثال (اختياري)
       btn.style.backgroundColor = "#9C1717";
 
-      // بعد ثانية، يروح للرابط فعليًا
       setTimeout(function () {
         window.location.href = btn.href;
       }, 1000);
